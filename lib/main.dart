63e3a7087bdff10bf964c2790e8c9b559f78b084
Sarfaraz Hussain <sarfarazsajjad@gmail.com>
2021-04-11 18:10:37 +0500

    battery info

---
+++ lib/main.dart
@@ -0,0 +1,105 @@
+import 'package:flutter/material.dart';
+import 'package:battery_plus/battery_plus.dart';
+import 'dart:async';
+
+void main() {
+  runApp(TabBarDemo());
+}
+
+class TabBarDemo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: DefaultTabController(
+        length: 3,
+        child: Scaffold(
+          appBar: AppBar(
+            bottom: TabBar(
+              tabs: [
+                Tab(icon: Icon(Icons.battery_std)),
+                Tab(icon: Icon(Icons.wifi)),
+                Tab(icon: Icon(Icons.device_hub)),
+              ],
+            ),
+            title: Text('System Info'),
+          ),
+          body: TabBarView(
+            children: [
+              MyHomePage(),
+              Icon(Icons.directions_transit),
+              Icon(Icons.directions_bike),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key? key, this.title}) : super(key: key);
+
+  final String? title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final Battery _battery = Battery();
+
+  BatteryState? _batteryState;
+  StreamSubscription<BatteryState>? _batteryStateSubscription;
+
+  @override
+  void initState() {
+    super.initState();
+    _batteryStateSubscription =
+        _battery.onBatteryStateChanged.listen((BatteryState state) {
+      setState(() {
+        _batteryState = state;
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Battery Status & Charge'),
+      ),
+      body: Center(
+        child: Text('$_batteryState'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.battery_unknown),
+        onPressed: () async {
+          final batteryLevel = await _battery.batteryLevel;
+          // ignore: unawaited_futures
+          showDialog<void>(
+            context: context,
+            builder: (_) => AlertDialog(
+              content: Text('Battery: $batteryLevel%'),
+              actions: <Widget>[
+                TextButton(
+                  child: const Text('OK'),
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                )
+              ],
+            ),
+          );
+        },
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    if (_batteryStateSubscription != null) {
+      _batteryStateSubscription!.cancel();
+    }
+  }
+}
